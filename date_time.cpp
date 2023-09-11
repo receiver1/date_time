afcad9e3176aa264c46504cf3619615569a380b6
Receiver <62743649+Receiver1@users.noreply.github.com>
2023-09-11 04:33:21 -0700

    Add files via upload

---
+++ date_time.cpp
@@ -0,0 +1,120 @@
+#include "date_time.h"
+
+using namespace receiver;
+
+time_zone::time_zone() : native_{0} {}
+
+time_zone::time_zone(value_type offset) : native_{offset} {}
+
+time_zone::time_zone(const std::string& timezone) : native_{0} {
+  DWORD index{0};
+  std::wstring timezone_name{timezone.begin(), timezone.end()};
+  DYNAMIC_TIME_ZONE_INFORMATION time_zone_info{};
+
+  while (EnumDynamicTimeZoneInformation(++index, &time_zone_info) !=
+         ERROR_NO_MORE_ITEMS) {
+    if (timezone_name.compare(time_zone_info.StandardName) != 0) continue;
+    native_ = time_zone_info.Bias * -60;
+    break;
+  }
+}
+
+time_zone receiver::time_zone::current() {
+  TIME_ZONE_INFORMATION time_zone_info{};
+  GetTimeZoneInformation(&time_zone_info);
+  return time_zone{time_zone_info.Bias * -60};
+}
+
+receiver::date_time::date_time() : native_{0}, zone_{} {}
+
+receiver::date_time::date_time(const std::string& datetime,
+                               date_format standard, const time_zone& zone)
+    : native_{0}, zone_{zone} {
+  SYSTEMTIME system_time{};
+
+  if (standard == date_format::ISO_8601) {
+    sscanf_s(datetime.c_str(), "%04u-%02u-%02uT%02u:%02u:%02u",
+             reinterpret_cast<std::uint32_t*>(&system_time.wYear),
+             reinterpret_cast<std::uint32_t*>(&system_time.wMonth),
+             reinterpret_cast<std::uint32_t*>(&system_time.wDay),
+             reinterpret_cast<std::uint32_t*>(&system_time.wHour),
+             reinterpret_cast<std::uint32_t*>(&system_time.wMinute),
+             reinterpret_cast<std::uint32_t*>(&system_time.wSecond));
+  }
+
+  FILETIME file_time{};
+  SystemTimeToFileTime(&system_time, &file_time);
+  native_ = file_time_to_timestamp(file_time) -
+            static_cast<value_type>(zone_.offset());
+}
+
+date_time::date_time(value_type timestamp, const time_zone& zone)
+    : native_{timestamp}, zone_{zone} {}
+
+date_time& date_time::set_time_zone(const time_zone& timezone) {
+  this->zone_ = timezone;
+  return *this;
+}
+
+date_time& date_time::operator+=(std::chrono::seconds seconds) {
+  this->native_ += seconds.count();
+  return *this;
+}
+
+date_time& date_time::operator-=(std::chrono::seconds seconds) {
+  this->native_ -= seconds.count();
+  return *this;
+}
+
+date_time& date_time::operator=(value_type timestamp) {
+  this->native_ = timestamp;
+  return *this;
+}
+
+std::string receiver::date_time::format(date_format standard,
+                                        bool consider_zone) {
+  std::string result{};
+  const auto file_time = timestamp_to_file_time(timestamp(consider_zone));
+  SYSTEMTIME system_time{};
+
+  FileTimeToSystemTime(&file_time, &system_time);
+
+  if (standard == date_format::ISO_8601) {
+    result.resize(21);
+    sprintf_s(&result[0], result.size(), "%04u-%02u-%02uT%02u:%02u:%02uZ",
+              system_time.wYear, system_time.wMonth, system_time.wDay,
+              system_time.wHour, system_time.wMinute, system_time.wSecond);
+  }
+
+  return result;
+}
+
+date_time receiver::date_time::current() {
+  FILETIME file_time{};
+  GetSystemTimeAsFileTime(&file_time);
+  return date_time{file_time_to_timestamp(file_time)};
+}
+
+date_time::value_type receiver::date_time::timestamp(bool consider_zone) const {
+  return native_ +
+         (consider_zone ? static_cast<value_type>(zone_.offset()) : 0);
+}
+
+auto receiver::date_time::file_time_to_timestamp(const FILETIME& file_time)
+    -> value_type {
+  LARGE_INTEGER time_parts{};
+  time_parts.LowPart = file_time.dwLowDateTime;
+  time_parts.HighPart = file_time.dwHighDateTime;
+
+  return (time_parts.QuadPart - UNIX_TIME_START) / TICKS_PER_SECOND;
+}
+
+auto receiver::date_time::timestamp_to_file_time(value_type timestamp)
+    -> FILETIME {
+  std::uint64_t ticks{timestamp * TICKS_PER_SECOND + UNIX_TIME_START};
+  FILETIME file_time{};
+  file_time.dwLowDateTime = (DWORD)ticks;
+  file_time.dwHighDateTime = ticks >> 32;
+
+  return file_time;
+}
---
+++ date_time.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#define WIN32_MEAN_AND_LEAN
+#include <Windows.h>
+#include <sysinfoapi.h>
+#include <timezoneapi.h>
+
+#include <chrono>
+#include <string>
+
+namespace receiver {
+enum date_format { ISO_8601 };
+
+class time_zone {
+ public:
+  using value_type = long;
+
+  /**
+   * Creates an instance of a class with zero offset.
+   */
+  time_zone();
+  /**
+   * Creates an instance of a class with the specified offset.
+   *
+   * @param offset Offset relative to UTC
+   */
+  time_zone(value_type offset);
+  /**
+   * Creates an instance of a class with the specified time zone.
+   * The time zone is taken from the Windows registry definitions.
+   *
+   * @param timezone Name of the time zone from the Windows registry. Example:
+   * Russia TZ 2 Standard Time
+   */
+  time_zone(const std::string& timezone);
+
+  static time_zone current();
+
+  value_type offset() const noexcept { return native_; }
+
+ private:
+  value_type native_;
+};
+
+class date_time {
+ public:
+  using value_type = long long;
+
+  /**
+   *  Creates an instance of a class with 01-01-1970 00:00:00 datetime.
+   */
+  date_time();
+  /**
+   * Creates an instance of a class using a string with date & time.
+   * The class does not parse milliseconds and zone offset.
+   *
+   * @param datetime Date with time in string format
+   * @param standard Standard for string processing
+   * @param zone Time zone of the specified date
+   */
+  date_time(const std::string& datetime,
+            date_format standard = date_format::ISO_8601,
+            const time_zone& zone = {});
+  /**
+   * Creates an instance of a class using a number with a date & time.
+   *
+   * @param timestamp Date with time in numeric format
+   * @param zone Time zone of the specified date
+   */
+  date_time(value_type timestamp, const time_zone& zone = {});
+
+  /**
+   * Sets the time zone for storing time.
+   *
+   * @return date_time Reference to this class instance
+   */
+  date_time& set_time_zone(const time_zone& timezone);
+
+  date_time& operator+=(std::chrono::seconds seconds);
+  date_time& operator-=(std::chrono::seconds seconds);
+  date_time& operator=(value_type timestamp);
+
+  /**
+   * Formats the stored time according to the specified standard.
+   *
+   * @param standard Formatting standard
+   * @param consider_zone Should we consider the time zone offset?
+   * @return std::string Formatted date in string format
+   */
+  std::string format(date_format standard = date_format::ISO_8601,
+                     bool consider_zone = true);
+
+  /**
+   * Returns initialized class with current time
+   * @return date_time
+   */
+  static date_time current();
+
+  /**
+   * Returns unix timestamp relative to 01-01-1970 00:00:00
+   * @return std::uint64_t
+   */
+  value_type timestamp(bool consider_zone = true) const;
+
+ private:
+  static constexpr std::uint64_t UNIX_TIME_START{0x019DB1DED53E8000};
+  static constexpr std::uint64_t TICKS_PER_SECOND{10000000};
+
+  static auto file_time_to_timestamp(const FILETIME& file_time) -> value_type;
+  static auto timestamp_to_file_time(value_type timestamp) -> FILETIME;
+
+ private:
+  value_type native_;  // offset relative to 01-01-1970 00:00:00
+  time_zone zone_;
+};
+}  // namespace receiver
